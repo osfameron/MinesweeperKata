c06c8b573d1031ee2df08995e9ae667311651c93
Hakim Cassimally <hakim.cassimally@gmail.com>
2020-07-31 20:33:37 +0100

    Move code into lib. Unpack and ToString

--- src/Minesweeper.cs
+++ src/Minesweeper.cs
@@ -1,11 +1,38 @@
+#define CONTRACTS_FULL
+
 using System;
+using System.Linq;
 
 namespace Minesweeper
 {
     public class Grid
     {
-        public Grid(string grid)
+        private char[][] grid;
+        public Grid(string picture)
         {
+            #region unpack and preconditions
+            // TODO: find standard idiom for this: Contract.Requires crashes `dotnet`
+            if (picture.Length == 0) {
+                throw new ArgumentException("Must pass in non-zero-length String");
+            }
+            string[] lines = picture.Split("\n");
+            var rows = from l in lines
+                       select l.ToCharArray();
+            var lenFirst = lines[0].Length;
+            if (rows.Any(r => r.Length != lenFirst))
+            {
+                throw new ArgumentException("Row size mismatch");
+            }
+            #endregion
+
+            grid = rows.ToArray();
+        }
+
+        public string ToString()
+        {
+            return (from r in grid
+                    select new string(r))
+                   .Aggregate((a,b) => $"{a}\n{b}");
         }
     }
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -9,47 +9,25 @@ namespace MinesweeperTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-            grid = new Grid(grid1);
-        }
         private Grid grid;
-        private const string grid1 = @"*...
+        private const string pic1 = @"*...
 ....
 .*..
 ....";
 
         [Test]
-        public void Test1()
+        public void TestInvalidArguments()
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(() => new Grid(""));
+            Assert.Throws<ArgumentException>(() => new Grid("short\nverylongline"));
         }
 
         [Test]
-        public void Test2()
+        public void TestBasic()
         {
-            string[] expected = { "*...", "....", ".*..", "...." };
-            Assert.AreEqual(expected, grid1.Split("\n"));
-
-            char[] exp2 = { 'f', 'o', 'o' };
-            Assert.AreEqual(exp2, "foo".ToCharArray());
-
-            var result =
-                (from line in "fo\nba".Split("\n")
-                 select line.ToCharArray())
-                .ToArray();
-            var lenFirst = result[0].Length;
-            Assert.True(result.All(s => s.Length == lenFirst));
-
-            Console.WriteLine(result);
-
-            Assert.AreEqual('f', result[0][0]);
-            Assert.AreEqual('o', result[0][1]);
-            Assert.AreEqual('b', result[1][0]);
-            Assert.AreEqual('a', result[1][1]);
-
+            var grid = new Grid(pic1);
+            Assert.IsNotNull(grid);
+            Assert.AreEqual(pic1, grid.ToString());
         }
-
     }
 }
